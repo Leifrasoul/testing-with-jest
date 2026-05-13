9e9c7651065b8b7368161437aa64647d20c574a7
Leifrasoul <leifrasoul@gmail.com>
2026-05-13 15:19:48 +0200

    Lade till eget Jest-test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,10 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop returns last value', () => {
+    stack.push(5);
+    stack.push(10);
+
+    expect(stack.pop()).toBe(5);
+});
